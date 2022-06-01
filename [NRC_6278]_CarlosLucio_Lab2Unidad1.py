dde08043877fd329f6495676cdf37e3d32a19e85
Celucio <75482093+Celucio@users.noreply.github.com>
2022-05-31 19:46:42 -0500

    Cambios realizados

--- [NRC_6278]_CarlosLucio_Lab2Unidad1.py
+++ [NRC_6278]_CarlosLucio_Lab2Unidad1.py
@@ -90,42 +90,7 @@ class Grafo():
 
 #Main
 if __name__ == "__main__":
-    #Instanciamos el objeto
-    g = Grafo(5, dirigido=False)
-
-    #Agregue bordes al grafo con peso predeterminado = 1
-    g.añadir_nodo(0, 1)
-    g.añadir_nodo(0, 2)
-    g.añadir_nodo(1, 2)
-    g.añadir_nodo(1, 4)
-    g.añadir_nodo(2, 3)
-
-    #Imprime el grafo generado en el formulario nodo n: {(nodo, peso)}
-    g.mostrar_lista_adyacencia()
-
-    #Muestra por medio de una lista las colas visitadas
-    print ("A continuación se muestra el recorrido primero en anchura (a partir del vértice 0)")
-    #Retorno de las colas visitadas 
-    g.bfs_transversal(0)
-    print()
-    print("Crear un nuevo grafo")
-    #Crearemos un nuevo grafo 1
-    g1 = Grafo(4, dirigido=True)
-    #Agregue bordes al grafo con peso predeterminado = 1
-    g1.añadir_nodo(0, 1)
-    g1.añadir_nodo(0, 2)
-    g1.añadir_nodo(0, 3)
-    g1.añadir_nodo(1, 2)
-    g1.añadir_nodo(1, 3)
-    g1.añadir_nodo(2, 3)
-    g1.añadir_nodo(3, 1)
-    #Imprime el grafo generado en el formulario nodo n: {(nodo, peso)}
-    g1.mostrar_lista_adyacencia()
-    #Muestra por medio de una lista las colas visitadas
-    print ("A continuación se muestra el recorrido primero en anchura (a partir del vértice 0)")
-    #Retorno de las colas visitadas
-    g1.bfs_transversal(0)
-    #Crearemos un nuevo grafo 2
+    
     print()
     print("Crear un nuevo grafo")
     #Instanciamos el nuevo objeto
